72762a6fe3b9de52cdb791a7bf208246fb2b2d5a
luolibin <luolibin@ablecloud.cn>
2016-12-19 20:05:45 +0800

    fix

--- funcs/procs.go
+++ funcs/procs.go
@@ -23,16 +23,16 @@ func ProcMetrics() (L []*model.MetricValue) {
 
 	pslen := len(ps)
 
-	for tags, curProc := range reportProcs {
+	for tags, preProc := range reportProcs {
 		cnt := 0
 		pids := map[int]struct{}{}
 		for i := 0; i < pslen; i++ {
-			if is_a(ps[i], curProc) {
+			if is_a(ps[i], preProc) {
 				cnt++
 				pids[ps[i].Pid] = struct{}{}
 			}
 		}
-		if cnt > 0 && is_restart_proc(pids, curProc) {
+		if cnt > 0 && is_restart_proc(pids, preProc) {
 			//此进程发生了重启,将进程数置为0
 			cnt = 0
 		}
@@ -41,41 +41,42 @@ func ProcMetrics() (L []*model.MetricValue) {
 	return
 }
 
-func is_restart_proc(pids map[int]struct{}, curProc *g.CacheProc) bool {
-	if len(curProc.Pids) != len(curProc.Pids) {
+func is_restart_proc(pids map[int]struct{}, preProc *g.CacheProc) bool {
+	if len(pids) != len(preProc.Pids) {
+		//进程数目前后不一致,则认为发生重启
 		return true
 	}
-	if len(curProc.Pids) == 1 {
-		if _, ok := curProc.Pids[-1]; ok {
+	if len(preProc.Pids) == 1 {
+		if _, ok := preProc.Pids[-1]; ok {
 			//此进程是新加的进程监控,无需对比两次采集的进程号
-			curProc.Pids = pids
+			preProc.Pids = pids
 			return false
 		}
 	}
 	flag := false
 	for pid, _ := range pids {
-		if _, ok := curProc.Pids[pid]; !ok {
+		if _, ok := preProc.Pids[pid]; !ok {
 			//由tag name、cmdline标识的同一个进程在两次采集的过程中发生了重启
 			flag = true
 			break
 		}
 	}
-	curProc.Pids = pids
+	preProc.Pids = pids
 	return flag
 }
 
-func is_a(p *nux.Proc, curProc *g.CacheProc) bool {
+func is_a(p *nux.Proc, preProc *g.CacheProc) bool {
 	// name
-	if len(curProc.Name) > 0 {
+	if len(preProc.Name) > 0 {
 		//进程监控设置了name tag
-		if p.Name != curProc.Name {
+		if p.Name != preProc.Name {
 			return false
 		}
 	}
 	// cmdline
-	if len(curProc.Cmdline) > 0 {
+	if len(preProc.Cmdline) > 0 {
 		//进程监控设置了cmdline tag
-		if p.Cmdline != curProc.Cmdline {
+		if p.Cmdline != preProc.Cmdline {
 			return false
 		}
 	}
