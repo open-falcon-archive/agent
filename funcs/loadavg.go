b6b829f8dc25f4f4c23b06275b176b4b9395ce42
zhangkun <zhangkun@yidian-inc.com>
2017-01-04 17:31:54 +0800

    采集cpu load数据时，增加tag：cpu_num=当前机器逻辑cpu个数

--- funcs/loadavg.go
+++ funcs/loadavg.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/open-falcon/common/model"
 	"github.com/toolkits/nux"
+	"runtime"
+	"strconv"
 )
 
 func LoadAvgMetrics() []*model.MetricValue {
@@ -13,11 +15,12 @@ func LoadAvgMetrics() []*model.MetricValue {
 		log.Println(err)
 		return nil
 	}
-
+	cpuNum := strconv.Itoa(runtime.NumCPU())
+	tagCpu := "cpu_num="+cpuNum
 	return []*model.MetricValue{
-		GaugeValue("load.1min", load.Avg1min),
-		GaugeValue("load.5min", load.Avg5min),
-		GaugeValue("load.15min", load.Avg15min),
+		GaugeValue("load.1min", load.Avg1min, tagCpu),
+		GaugeValue("load.5min", load.Avg5min, tagCpu),
+		GaugeValue("load.15min", load.Avg15min, tagCpu),
 	}
 
 }
